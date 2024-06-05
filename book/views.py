83040f821e40662503937c6e95a31a26cfe4b3b8
sakami <3209996299@qq.com>
2024-06-05 20:45:09 +0800

    视图函数

--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+视图，就是python函数
+视图函数的2个要求：
+    1.视图函数第一个参数是接受请求
+"""
+from django.http import HttpRequest, HttpResponse
+# 通过localhost:8000/index/来访问视图函数
+def index(request):
+
+    return HttpResponse('ok')
