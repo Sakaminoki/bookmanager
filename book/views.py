2975c07aca9463516000a1b1d9fc6062f03af300
sakami <3209996299@qq.com>
2024-06-06 21:03:50 +0800

    http模板使用

--- book/views.py
+++ book/views.py
@@ -10,4 +10,15 @@ from django.http import HttpRequest, HttpResponse
 # 通过localhost:8000/index/来访问视图函数
 def index(request):
 
-    return HttpResponse('ok')
+    # return HttpResponse('ok')
+    # request, template_name, context=None
+    # request,请求
+    # template name,模板名字
+    # context=None,将视图中的数据传递给html模板，采用{{字典的键名}}显示数据
+
+    # 模拟数据查询
+    context={
+        'name':'context使用'
+    }
+
+    return render(request, 'book/index.html',context=context)
--- bookmanager/settings.py
+++ bookmanager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 import book.apps
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,10 +57,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bookmanager.urls'
 
+#模板配置
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 模板文件目录
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
